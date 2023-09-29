564443f53c48d86f2f7d258b56aaca66bd635740
onlyeatwater <63968524+onlyeatwater@users.noreply.github.com>
2023-09-28 20:57:25 -0400

    new main app

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# @app.get("/")
+# async def root():
+#     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+from typing import Union
+
+# app = FastAPI()
+
+
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+async def read_root():
+    return {"Hello World": "Main landing page"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+ 
